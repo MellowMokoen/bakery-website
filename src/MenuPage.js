30a2df044c36a79811df4fc4ca8de59ff6776358
MellowMokoen <mamello3632@gmail.com>
2023-11-10 12:16:00 +0200

    Created and imported new css components

--- src/MenuPage.js
+++ src/MenuPage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./MenuPage.css";
 
 export default function() {
     return (
--- src/index.js
+++ src/index.js
@@ -2,18 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import HomePage from "./HomePage";
-import AboutPage from "./AboutPage";
-import MenuPage from "./MenuPage";
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <HomePage />
-    <AboutPage />
-    <MenuPage />
+    
   </React.StrictMode>
 );
 
