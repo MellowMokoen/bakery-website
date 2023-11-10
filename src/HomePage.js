3540a2377f32a68dd252100a52d6f90cf254817f
MellowMokoen <mamello3632@gmail.com>
2023-11-10 12:03:23 +0200

    Created new components and added images

---
+++ src/HomePage.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./HomePage.css";
+
+
+export default function() {
+    return (
+        <div>
+            <header>
+                <ul>
+                    <li>
+                  Home 
+                    </li>
+                    <li>
+                  About 
+                    </li>
+                    <li>
+                  Menu
+                    </li>
+                </ul>
+            </header>
+            <h1>
+                Mellow's Bakery
+            </h1>
+            <h2>
+                Bakery
+            </h2>
+        </div>
+    );
+}
---
+++ src/MenuPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function() {
+    return (
+        <h4>Delicious menu</h4>
+    );
+}
