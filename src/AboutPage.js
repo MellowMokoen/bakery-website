cc83a96fa81734f08bb96a7ba81292a9057ff220
MellowMokoen <mamello3632@gmail.com>
2023-11-16 23:55:41 +0200

    Updated about page and styled it

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -3,6 +3,27 @@ import "./AboutPage.css";
 
 export default function(){
     return (
-        <h3>About Mellow's Bakery</h3>
+      <div>
+        <div className="about-h1">
+          About Mellow's Bakery
+        </div>
+        <div className="about-p">
+            Mellow's Bakery, a cherished establishment rooted in the heart of the community, <br />
+             embarked on its culinary journey with a simple yet profound mission — <br /> 
+             to create delightful moments through the art of baking. <br /> 
+             Guided by a commitment to craft exceptional treats that transcend the ordinary, <br />
+             Mellow's Bakery aspires to be the go-to destination for those seeking a symphony of flavors in every bite.
+            <br />
+            <br />
+            Built upon a rich history of family traditions and a love for the baking craft, <br />
+            Mellow's Bakery has evolved into a beloved institution since its inception. <br />
+            Upholding values of integrity, community engagement, and continuous innovation, <br />
+            the bakery has cultivated a warm and inviting atmosphere where patrons can savor both <br /> 
+            the sweetness of its treats and the genuine care that goes into each recipe. <br />
+            Through the years, Mellow's has not only perfected the art of baking but has also <br />
+            become an integral part of the local tapestry, weaving memories, and connections that <br />
+            endure beyond the confines of its charming storefront.
+        </div>
+      </div>
     );
 }
