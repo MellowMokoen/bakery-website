af69c8db54ebb5aea87922b120e1cff0a8184d96
MellowMokoen <mamello3632@gmail.com>
2023-11-17 00:29:57 +0200

    Added container background img for homepage

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,7 +3,7 @@ import "./HomePage.css";
 
 export default function() {
     return (
-        <div>
+        <div className="container" id="home">
           <h1 className="Home-h1">
             Welcome to Mellow's Bakery
           </h1> 
